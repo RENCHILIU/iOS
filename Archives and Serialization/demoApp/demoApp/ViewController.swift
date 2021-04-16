0a75f1620d534e5c7b0d7d3663807a6c428e9318
RenchiLiu <raylrc@gmail.com>
2021-04-16 14:08:39 -0500

    project

---
+++ Archives and Serialization/demoApp/demoApp/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  demoApp
+//
+//  Created by liurenchi on 4/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    let request = URLRequest(url: URL(string: "https://reqres.in/api/users?page=2")!)
+    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+      
+      print(data)
+      
+      print(response)
+      
+      print(error)
+      
+      
+      
+    }
+    
+    task.resume()
+  }
+
+
+}
+
